ba26661805e6748477618a4c1a968a6504fcd892
edgar.schnueriger <edgar.schnueriger@ricardo.ch>
2016-06-09 10:49:51 +0200

    make legacy typo-elements fallback work

--- src/legacy.js
+++ src/legacy.js
@@ -1,3 +1,4 @@
 var button = require('elements/legacy'),
     panel = require('panel/legacy'),
-    pricebox = require('pricebox/legacy');
+    pricebox = require('pricebox/legacy'),
+    typo = require('typo-elements/legacy');
