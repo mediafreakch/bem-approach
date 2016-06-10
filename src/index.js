443c04a46aea5f6db9ab911a803416f75ea5ff00
edgar.schnueriger <edgar.schnueriger@ricardo.ch>
2016-06-10 14:10:06 +0200

    Squashed commit of the following:
    commit e04446a56e8d579daeb98c9c8cf011ae4f1c0c12
    Merge: 5312fb1 f342ac6
    Author: edgar.schnueriger <edgar.schnueriger@ricardo.ch>
    Date:   Fri Jun 10 14:01:34 2016 +0200
    
        Merge branch 'master' into routing
    
    commit 5312fb1bdec14f92c9787e54bfad358d80fde2b8
    Author: edgar.schnueriger <edgar.schnueriger@ricardo.ch>
    Date:   Fri Jun 10 13:59:21 2016 +0200
    
        style images with element spacing at bottom
    
    commit 9ec4d6d98e678e497ee14cb3a77c548744838d3f
    Author: edgar.schnueriger <edgar.schnueriger@ricardo.ch>
    Date:   Fri Jun 10 13:59:03 2016 +0200
    
        include boxmodel overwrite
    
    commit 83ee81c50ca16c781c25e5f344396664c2942149
    Author: edgar.schnueriger <edgar.schnueriger@ricardo.ch>
    Date:   Fri Jun 10 13:58:13 2016 +0200
    
        use REM sizes for element and component spacing
    
    commit ebbd330914631379cfdd8cb8eecd131c84af3637
    Author: edgar.schnueriger <edgar.schnueriger@ricardo.ch>
    Date:   Fri Jun 10 13:57:46 2016 +0200
    
        use correct mixins to apply element and component spacing
    
    commit 4ed03b36e47be0c177b87013d5d2bd14536455ba
    Author: edgar.schnueriger <edgar.schnueriger@ricardo.ch>
    Date:   Fri Jun 10 13:56:32 2016 +0200
    
        cleanup element and component spacing helper
    
    commit 28afb2872a91ae181873072e85c40b1771e1bdd2
    Author: edgar.schnueriger <edgar.schnueriger@ricardo.ch>
    Date:   Fri Jun 10 13:49:00 2016 +0200
    
        added routing and splitted icons + typography to separate pages
    
    commit 26bdd50b0f32d42d594de8932a8332769d534c5d
    Author: edgar.schnueriger <edgar.schnueriger@ricardo.ch>
    Date:   Fri Jun 10 13:46:23 2016 +0200
    
        component spacing for body
    
    commit 6db9d7b9c8e198880e7ce39d238c7df35f02e538
    Merge: d810532 f0a574f
    Author: edgar.schnueriger <edgar.schnueriger@ricardo.ch>
    Date:   Fri Jun 10 10:32:13 2016 +0200
    
        Merge branch 'master' into prosq-5
    
    commit d8105329c79d4a216cd6de3d15de1ea88162b406
    Author: edgar.schnueriger <edgar.schnueriger@ricardo.ch>
    Date:   Fri Jun 10 10:28:10 2016 +0200
    
        apply REM sizing for different breakpoints. Added list variations. Changed headings css selector and added link state stylings
    
    commit 5585425fbdbcd32fc3f3467abe848b4375d6d7ff
    Author: edgar.schnueriger <edgar.schnueriger@ricardo.ch>
    Date:   Fri Jun 10 10:26:01 2016 +0200
    
        added root font size units for REM approach. changed default-font-size to REM. Added font-size variations
    
    commit 423c952f02b1bf25a30bf462c63f43518b312152
    Author: edgar.schnueriger <edgar.schnueriger@ricardo.ch>
    Date:   Fri Jun 10 10:22:36 2016 +0200
    
        added a text-sample package to the mix to illustrate how text styles get applied

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-var Vue = require('vue');
-var app = require('./app.vue');
+var Vue = require('vue'),
+    VueRouter = require('vue-router'),
+    routing = require('app-routing'),
+    app = require('./app.vue');
 
-new Vue({
-  el: 'body',
-  components: { app: app }
-});
+// install Router first
+Vue.use(VueRouter);
+
+// use Router
+var router = new VueRouter();
+router.map(routing);
+
+// init app using our router
+var routeHandler = router.start(app, 'body');
