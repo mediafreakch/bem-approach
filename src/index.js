29b793f6ecc74f08d8028918e81e1d93b38ec8e3
edgar.schnueriger <edgar.schnueriger@ricardo.ch>
2016-06-16 14:11:39 +0200

    organize sub-routes in its own packages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 var Vue = require('vue'),
     VueRouter = require('vue-router'),
     routing = require('app~routing'),
+    VueResource = require('vue-resource'),
     app = require('./app.vue');
 
 // install Router first
 Vue.use(VueRouter);
+
+// install REST API vue plugin
+Vue.use(VueResource);
 
 // use Router
 var router = new VueRouter();
