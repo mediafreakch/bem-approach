250dd10124bdffc4ed483edb12cf665dfff98887
edgar.schnueriger <edgar.schnueriger@ricardo.ch>
2016-06-24 12:09:11 +0200

    added footer & header

--- src/app.vue
+++ src/app.vue
@@ -1,17 +1,23 @@
 <template>
-  <div id="app">
+  <r-header></r-header>
+  <div id="app" class="row box-component">
     <r-navigation></r-navigation>
     <router-view></router-view>
   </div>
+  <r-footer></r-footer>
 </template>
 
 <script>
   var navigation = require('component~navigation'),
+      header = require('component~header'),
+      footer = require('component~footer'),
       typo = require('element~typo');
 
   module.exports = {
     components: {
-      "r-navigation": navigation
+      'r-navigation': navigation,
+      'r-header': header,
+      'r-footer': footer
     },
     replace: false
   };
